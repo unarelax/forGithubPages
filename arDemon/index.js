fe7c2e0bc2878139036253b8378037fbf542f0d4
yuri12138 <yuri_25@163.com>
2023-07-21 16:51:12 +0800

    666

--- arDemon/index.js
+++ arDemon/index.js
@@ -1,45 +1,144 @@
-import * as THREE from "three";
-import * as THREEx from "./node_modules/@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js";
+import * as THREE from 'three';
+import * as THREEx from './node_modules/@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js';
 
 function main() {
-  const canvas = document.getElementById("canvas1");
+    const canvas = document.getElementById('canvas1');
 
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
-  const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
+    const renderer = new THREE.WebGLRenderer({canvas: canvas});
 
-  const arjs = new THREEx.LocationBased(scene, camera);
-  const cam = new THREEx.WebcamRenderer(renderer);
+    const arjs = new THREEx.LocationBased(scene, camera);
+    const cam = new THREEx.WebcamRenderer(renderer);
 
-  const geom = new THREE.BoxGeometry(50, 50, 50);
-  const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const box = new THREE.Mesh(geom, mtl);
+    const geom = new THREE.BoxGeometry(20, 20, 20);
+    const mtl = new THREE.MeshBasicMaterial({color: 0xff0000});
 
-  // Create the device orientation tracker
-  const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
+    const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
 
-  // Change this to a location close to you (e.g. 0.001 degrees of latitude north of you)
-  arjs.add(box, 120.191, 30.151);
+    let fetched = false;
 
-  arjs.startGps();
+    // Handle the "gpsupdate" event on the LocationBased object
+    // This triggers when a GPS update (from the Geolocation API) occurs
+    // 'pos' is the position object from the Geolocation API.
 
-  requestAnimationFrame(render);
+    arjs.on("gpsupdate", async(pos) => {
+        if(!fetched) {
+            const response = await fetch(`https://hikar.org/webapp/map?bbox=${pos.coords.longitude-0.01},${pos.coords.latitude-0.01},${pos.coords.longitude+0.01},${pos.coords.latitude+0.01}&layers=poi&outProj=4326`);
 
-  function render() {
-    if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
-      renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-      const aspect = canvas.clientWidth / canvas.clientHeight;
-      camera.aspect = aspect;
-      camera.updateProjectionMatrix();
-    }
+            const geojson = await response.json();
 
-    // Update the scene using the latest sensor readings
-    deviceOrientationControls.update();
+            geojson.features.forEach ( feature => {
+                const box = new THREE.Mesh(geom, mtl);
+                arjs.add(box, feature.geometry.coordinates[0], feature.geometry.coordinates[1]);            
+            });
 
-    cam.update();
-    renderer.render(scene, camera);
+            fetched = true;
+        }
+    });
+
+    arjs.startGps();
+
     requestAnimationFrame(render);
-  }
+
+    function render() {
+        if(canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
+            renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+            const aspect = canvas.clientWidth/canvas.clientHeight;
+            camera.aspect = aspect;
+            camera.updateProjectionMatrix();
+        }
+
+        // Update the scene using the latest sensor readings
+        deviceOrientationControls.update();
+
+        cam.update();
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import * as THREE from "three";
+// import * as THREEx from "./node_modules/@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js";
+
+// function main() {
+//   const canvas = document.getElementById("canvas1");
+
+//   const scene = new THREE.Scene();
+//   const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
+//   const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+
+//   const arjs = new THREEx.LocationBased(scene, camera);
+//   const cam = new THREEx.WebcamRenderer(renderer);
+
+//   const geom = new THREE.BoxGeometry(50, 50, 50);
+//   const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+//   const box = new THREE.Mesh(geom, mtl);
+
+//   // Create the device orientation tracker
+//   const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
+
+//   // Change this to a location close to you (e.g. 0.001 degrees of latitude north of you)
+//   arjs.add(box, 120.191, 30.151);
+
+//   arjs.startGps();
+
+//   requestAnimationFrame(render);
+
+//   function render() {
+//     if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
+//       renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+//       const aspect = canvas.clientWidth / canvas.clientHeight;
+//       camera.aspect = aspect;
+//       camera.updateProjectionMatrix();
+//     }
+
+//     // Update the scene using the latest sensor readings
+//     deviceOrientationControls.update();
+
+//     cam.update();
+//     renderer.render(scene, camera);
+//     requestAnimationFrame(render);
+//   }
 
   //   arjs.add(box, -0.72, 51.051);
 
@@ -88,6 +187,6 @@ function main() {
   //     renderer.render(scene, camera);
   //     requestAnimationFrame(render);
   //   }
-}
+// }
 
-main();
+// main();
